34e1506cb53df106bf93a94254723ba04d45d2ee
Esa-Petri Tirkkonen <esereja@yahoo.co.uk>
2020-10-29 15:51:41 +0200

    groups and user work like intended
    VIP,FIX,BROKEN,WORKS,
    More detailed explanatory text, if necessary. Wrap it to about 72
    characters or so. In some contexts, the first line is treated as the
    subject of the commit and the rest of the text as the body. The
    blank line separating the summary from the body is critical (unless
    you omit the body entirely); various tools like `log`, `shortlog`
    and `rebase` can get confused if you run the two together.
    
    Explain the problem that this commit is solving. Focus on why you
    are making this change as opposed to how (the code explains that).
    Are there side effects or other unintuitive consequences of this
    change? Here's the place to explain them.
    
    Further paragraphs come after blank lines.
    
     - Bullet points are okay, too
    
     - Typically a hyphen or asterisk is used for the bullet, preceded
       by a single space, with blank lines in between, but conventions
       vary here
    
    If you use an issue tracker, put references to them at the bottom,
    like this:
    
    Resolves: #123
    See also: #456, #789
    [Ticket: X]

--- scim_server/test.py
+++ scim_server/test.py
@@ -3,105 +3,124 @@ import uuid
 import datetime
 import json
 
-# class that defines user
+
 class User:
+    """
+    class that defines user
+    """
     port = 0
     hostname = ""
 
-    #meta data, these are either constant or automatic
+    # meta data, these are either constant or automatic
     schema = "schemas:[urn:ietf:params:scim:schemas:core:2.0:User]"
     resourceType = "User"
     created = str(datetime.datetime.now())
-    lastModified= str(datetime.datetime.now())
+    lastModified = str(datetime.datetime.now())
     version = 0
 
-    #user id
+    # user id
     id = str(uuid.uuid4())
-    externalId = "" #id used in external system
+    externalId = ""  # id used in external system
 
     # default constructor
-    def __init__(self,portIn=0,hostNameIn="",appVersionIN=0):
+    def __init__(self, portIn=0, hostNameIn="", appVersionIn=0):
+        """
+
+        :param portIn:
+        :param hostNameIn:
+        :param appVersionIn:
+        """
         self.port = portIn
         self.hostname = hostNameIn
         self.version = appVersionIn
 
-    #location metadata
-    location = "http://"+self.hostname+":"+str(self.port)+"/v2/Users/"+str(self.id)
+    # location metadata
+    location = "http://" + hostname + ":" + str(port) + "/v2/Users/" + str(id)
 
-    #user data
-    socialSecurityCode = "" #221079-123A
-    familyName = "" #Koskinen
-    firstNames = "" #Keijo Esko Petri
-    nickName = ""   #kamu
-    address = ""    #Jaakonkatu 12 A 3
-    postalCode = "" #52100
-    PostalDistrict = "" #Helsinki
+    # user data
+    username = ""  # admin,root,keijo
+    socialSecurityCode = ""  # 221079-123A
+    familyName = ""  # Koskinen
+    firstNames = ""  # Keijo Esko Petri
+    nickName = ""  # kamu
+    address = ""  # Jaakonkatu 12 A 3
+    postalCode = ""  # 52100
+    PostalDistrict = ""  # Helsinki
 
-    LanguageCode = "" #FI
-    LanguageCodeDescription = "" #FINLAND
+    LanguageCode = ""  # FI
+    LanguageCodeDescription = ""  # FINLAND
 
-    #work period data
+    # work period data
     workPeriodId = str(uuid.uuid4())
-    workPeriodDescription = "" #Osaston siivooja
-    OficialJobDescription = "" #Siivooja
-    workDepartment = "" #osaston nimi
+    workPeriodDescription = ""  # Osaston siivooja
+    OficialJobDescription = ""  # Siivooja
+    workDepartment = ""  # osaston nimi
 
     workPeriodStartDate = ""
     workPeriodEndDate = ""
-    workPeriodStatus = False #true = working, false = period ended
+    workPeriodStatus = False  # true = working, false = period ended
 
-    isSupervisor= False # is this person supervisor to somebody
-    idOfSupervisor = "" # uuid of supervisor
+    isSupervisor = False  # is this person supervisor to somebody
+    idOfSupervisor = ""  # uuid of supervisor
 
-    def dataLoad(self,data):
-            self.id = data['id']
-            self.externalId = data['externalId']
+    def dataLoad(self, data):
+        """
+        dataLoad
+        :param data:
+        """
+        self.id = data['id']
+        self.externalId = data['externalId']
 
-            self.resourceType = data['meta']['resourceType']
-            self.created = data['meta']['created']
-            self.lastModified = data['meta']['lastModified']
-            self.location = data['meta']['location']
-            self.version = data['meta']['version']
+        self.resourceType = data['meta']['resourceType']
+        self.created = data['meta']['created']
+        self.lastModified = data['meta']['lastModified']
+        self.location = data['meta']['location']
+        self.version = data['meta']['version']
 
-            self.socialSecurityCode = data['socialSecurityCode']
-            self.familyName = data['familyName']
-            self.firstNames = data['firstNames']
-            self.nickName = data['nickName']
-            self.address = data['address']
-            self.postalCode = data['postalCode']
-            self.PostalDistrict = data['PostalDistrict']
-            self.LanguageCode = data['LanguageCode']
-            self.LanguageCodeDescription = data['LanguageCodeDescription']
-            self.workPeriodId = data['workPeriodId']
-            self.workPeriodDescription = data['workPeriodDescription']
-            self.OficialJobDescription = data['OficialJobDescription']
-            self.workDepartment = data['workDepartment']
-            self.workPeriodStartDate = data['workPeriodStartDate']
-            self.workPeriodEndDate = data['workPeriodEndDate']
-            self.workPeriodStatus = data['workPeriodStatus']
-            self.isSupervisor = data['isSupervisor']
-            self.idOfSupervisor = data['idOfSupervisor']
+        self.username = data['username']
+        self.socialSecurityCode = data['socialSecurityCode']
+        self.familyName = data['familyName']
+        self.firstNames = data['firstNames']
+        self.nickName = data['nickName']
+        self.address = data['address']
+        self.postalCode = data['postalCode']
+        self.PostalDistrict = data['PostalDistrict']
+        self.LanguageCode = data['LanguageCode']
+        self.LanguageCodeDescription = data['LanguageCodeDescription']
+        self.workPeriodId = data['workPeriodId']
+        self.workPeriodDescription = data['workPeriodDescription']
+        self.OficialJobDescription = data['OficialJobDescription']
+        self.workDepartment = data['workDepartment']
+        self.workPeriodStartDate = data['workPeriodStartDate']
+        self.workPeriodEndDate = data['workPeriodEndDate']
+        self.workPeriodStatus = data['workPeriodStatus']
+        self.isSupervisor = data['isSupervisor']
+        self.idOfSupervisor = data['idOfSupervisor']
 
     def dataDump(self):
-        data ={
+        """
+        dataDump
+        """
+        data = {
             "schema": self.schema,
-            "id":  self.id,
-            "externalId":  self.externalId,
-            "meta":{
-                "resourceType":  self.resourceType,
-                "created":  self.created,
-                "lastModified":  self.lastModified,
-                "location":  self.location,
-                "version":  self.version,
+            "id": self.id,
+            "externalId": self.externalId,
+            "meta": {
+                "resourceType": self.resourceType,
+                "created": self.created,
+                "lastModified": self.lastModified,
+                "location": self.location,
+                "version": self.version,
             },
-            "socialSecurityCode":  self.socialSecurityCode,
-            "familyName":  self.familyName,
-            "firstNames":  self.firstNames,
-            "nickName":  self.nickName,
-            "address":  self.address,
-            "postalCode":  self.postalCode,
-            "PostalDistrict":  self.PostalDistrict,
-            "LanguageCode":  self.LanguageCode,
+            "username": self.username,
+            "socialSecurityCode": self.socialSecurityCode,
+            "familyName": self.familyName,
+            "firstNames": self.firstNames,
+            "nickName": self.nickName,
+            "address": self.address,
+            "postalCode": self.postalCode,
+            "PostalDistrict": self.PostalDistrict,
+            "LanguageCode": self.LanguageCode,
             "LanguageCodeDescription": self.LanguageCodeDescription,
             "workPeriodId": self.workPeriodId,
             "workPeriodDescription": self.workPeriodDescription,
@@ -115,105 +134,145 @@ class User:
         }
         return data
 
-# class that defines groups
-class group:
+
+class Group:
     port = 0
     hostname = ""
 
-    #meta data, these are either constant or automatic
+    # meta data, these are either constant or automatic
     schema = "schemas:[urn:ietf:params:scim:schemas:core:2.0:Group]"
     resourceType = "User"
     created = str(datetime.datetime.now())
-    lastModified= str(datetime.datetime.now())
+    lastModified = str(datetime.datetime.now())
     version = 0
 
-    #user id
+    # user id
     id = str(uuid.uuid4())
-    externalId = "" #id used in external system
+    externalId = ""  # id used in external system
 
-    # default constructor
-    def __init__(self,portIn=0,hostNameIn="",appVersionIN=0):
+    def __init__(self, portIn=0, hostNameIn="", appVersionIn=0):
+        """
+        default constructor
+        :param portIn:
+        :param hostNameIn:
+        :param appVersionIn:
+        """
         self.port = portIn
         self.hostname = hostNameIn
         self.version = appVersionIn
 
-    #location metadata
-    location = "http://"+self.hostname+":"+str(self.port)+"/v2/Groups/"+str(self.id)
+    # location metadata
+    location = "http://" + hostname + ":" + str(port) + "/v2/Groups/" + str(id)
 
-    displayName = "" # "Sales Reps",
-    members = [] #List of users in this group
+    displayName = ""  # "Sales Reps",
+    members = []  # List of users in this group
 
-    """
+    def IsMemember(self, id):
+        """
+
     Check if user is allready in members list
-    """
-    def IsMemeber(self,id):
+        :param id:
+        :return:
+        """
         if id in self.members:
             return True
         return False
 
-    """
-    add user to members list
-    """
-    def addMember(self,id):
-        if IsMemeber(id):
+    def addMember(self, id):
+        """
+        add user to members list
+        :param id:
+        :return:
+        """
+        if self.IsMemember(id):
             return False
-        else
+        else:
             self.members.append(id)
         return True
 
-    """
-    remove user from members list
-    """
     def dellMember(self):
-        if IsMemeber(id):
+        """
+        remove user from members list
+        :return:
+        """
+        if self.IsMemember(id):
+            self.members.remove(id)
+        else:
             return False
-        else
-            self.members.append(id)
         return True
 
-    """
-    create coma separated string from members list
-    """
-    def membersToString(self)
+    def membersToString(self):
+        """
+        members list into string
+        :return:
+        """
         strOut = ""
+        i = 0
         for m in self.members:
-            strOut = strOut + "," + str(m)
+            if i > 0:
+                strOut = strOut + "," + str(m)
+            else:
+                strOut = str(m)
+            i = i + 1
         return strOut
 
     def dataDump(self):
-        data ={
+        """
+        dataDump
+        :return:
+        """
+        data = {
             "schema": self.schema,
-            "id":  self.id,
-            "externalId":  self.externalId,
-            "meta":{
-                "resourceType":  self.resourceType,
-                "created":  self.created,
-                "lastModified":  self.lastModified,
-                "location":  self.location,
-                "version":  self.version,
+            "id": self.id,
+            "externalId": self.externalId,
+            "meta": {
+                "resourceType": self.resourceType,
+                "created": self.created,
+                "lastModified": self.lastModified,
+                "location": self.location,
+                "version": self.version,
             },
             "displayName": self.displayName,
-            "members":[self.membersToString()]
+            "members": self.members
         }
         return data
 
+    def dataLoad(self, data):
+        """
+        dataload
+        :rtype: object
+        """
+        self.schema = data['schema']
+        self.id = data['id']
+        self.externalId = data['externalId']
+        self.resourceType = data['meta']['resourceType']
+        self.created = data['meta']['created']
+        self.lastModified = data['meta']['lastModified']
+        self.location = data['meta']['location']
+        self.version = data['meta']['version']
+        self.displayName = data['displayName']
+        self.members = data['members']
 
 
-#TODO link to main program
-PORT=4555
-HOSTNAME="localhost"
-APP_VERSION=0.1
+# TODO link to main program
+PORT = 4555
+HOSTNAME = "localhost"
+APP_VERSION = 0.1
 
-#user test
-newUser = User(PORT,HOSTNAME,APP_VERSION)
+print("\nuser test")
+# user test
+newUser = User(PORT, HOSTNAME, APP_VERSION)
 data_dump = json.dumps(newUser.dataDump(), indent=4)
 print(data_dump)
-
 newUser.dataLoad(json.loads(data_dump))
-print( json.dumps(newUser.dataDump()) )
+print(json.dumps(newUser.dataDump()))
 
-
-#group test
-newgroup = Group(PORT,HOSTNAME,APP_VERSION)
-data_dump = json.dumps(newUser.dataDump(), indent=4)
+print("\ngroup test")
+# group test
+newgroup = Group(PORT, HOSTNAME, APP_VERSION)
+newgroup.addMember("1")
+newgroup.addMember("2")
+data_dump = json.dumps(newgroup.dataDump(), indent=4)
 print(data_dump)
+newgroup.dataLoad(json.loads(data_dump))
+print(json.dumps(newgroup.dataDump()))
